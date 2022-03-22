71afdae2788e5fb641c2439e25039a6c0d3ab9d6
semminho <semmi.ezaitouni@gmail.com>
2022-03-22 10:16:24 +0100

    crud updated put en added delete

--- routes/restricted-route.js
+++ routes/restricted-route.js
@@ -12,37 +12,57 @@ router.get("/users/:id",basicAuth.authRouting.admin, mijndb.getGebruiker);
 
 router.get("/bestellingen",basicAuth.authRouting.admin, mijndb.getAllBestellingen);
 router.get("/bestellingen/:id",basicAuth.authRouting.admin, mijndb.getBestellingById);
-router.post("/bestellingen",basicAuth.authRouting.admin, mijndb.setIngredient);
-
+router.post("/bestellingen",basicAuth.authRouting.admin, mijndb.setBestelling);
+router.put("/bestellingen/:id",basicAuth.authRouting.admin, mijndb.updateBestelling);
+router.delete("/bestellingen/:id",basicAuth.authRouting.admin, mijndb.deleteBestelling);
 
 router.get("/producten",basicAuth.authRouting.admin, mijndb.getAllProducten);
 router.get("/producten/:id",basicAuth.authRouting.admin, mijndb.getProductById);
 router.post("/producten",basicAuth.authRouting.admin, mijndb.setProduct);
+router.put("/producten/:id",basicAuth.authRouting.admin, mijndb.updateProduct);
+router.delete("/producten/:id",basicAuth.authRouting.admin, mijndb.deleteProduct);
+
+
 
 router.get("/afrekening",basicAuth.authRouting.admin, mijndb.getAllAfrekeningen);
 router.get("/afrekening/:id",basicAuth.authRouting.admin, mijndb.getAfrekeningById);
 router.post("/afrekening",basicAuth.authRouting.admin, mijndb.setAfrekening);
+router.put("/afrekening/:id",basicAuth.authRouting.admin, mijndb.updateAfrekening);
+router.delete("/afrekening/:id",basicAuth.authRouting.admin, mijndb.deleteAfrekening);
 
 
 
 router.get("/ingredienten/:id",basicAuth.authRouting.admin, mijndb.getIngredientByID);
 router.get("/ingredienten",basicAuth.authRouting.admin, mijndb.getAllIngredienten);
 router.post("/ingredienten",basicAuth.authRouting.admin, mijndb.setIngredient);
+router.put("/ingredienten/:id",basicAuth.authRouting.admin, mijndb.updateIngredient);
+router.delete("/ingredienten/:id",basicAuth.authRouting.admin, mijndb.deleteIngredient);
+
 
 
 router.get("/samenstelling/:id",basicAuth.authRouting.admin, mijndb.getSamenstellingById);
 router.get("/samenstelling",basicAuth.authRouting.admin, mijndb.getAllSamenstelling);
 router.post("/samenstelling",basicAuth.authRouting.admin, mijndb.setSamenstelling);
+router.put("/samenstelling/:id",basicAuth.authRouting.admin, mijndb.updateSamenstelling);
+router.delete("/samenstelling/:id",basicAuth.authRouting.admin, mijndb.deleteSamenstelling);
+
+
 
 
 router.get("/suballergenen/:id",basicAuth.authRouting.admin, mijndb.getSuballergeenById);
 router.get("/suballergenen",basicAuth.authRouting.admin, mijndb.getSuballergenen);
 router.post("/suballergenen",basicAuth.authRouting.admin, mijndb.setSuballergeen);
+router.put("/suballergenen/:id",basicAuth.authRouting.admin, mijndb.updateSuballergeen);
+router.delete("/suballergenen/:id",basicAuth.authRouting.admin, mijndb.deleteSuballergeen);
+
 
 
 router.get("/subtypes/:id",basicAuth.authRouting.admin, mijndb.getSubTypeById);
 router.get("/subtypes",basicAuth.authRouting.admin, mijndb.getAllSubTypes);
 router.post("/subtypes",basicAuth.authRouting.admin, mijndb.setSubType);
+router.put("/subtypes/:id",basicAuth.authRouting.admin, mijndb.updateSubType);
+router.delete("/subtypes/:id",basicAuth.authRouting.admin, mijndb.deleteSubType);
+
 
 
 
