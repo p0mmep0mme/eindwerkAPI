1992290fabe51ff5f259f14d50d791d0517c383c
p0mmep0mme <93192139+p0mmep0mme@users.noreply.github.com>
2022-03-24 14:27:13 +0100

    postgress Pool

--- database.js
+++ database.js
@@ -1,11 +1,15 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-    "host": "ec2-99-80-170-190.eu-west-1.compute.amazonaws.com",
-    "database": "d69013s2v5ealh", 
-    "user": "kuklpsxjydnblu",
-    "port":5432,
-    "password": "eef8b90546b3d90f332de10020bf44002ada6a142ee8eaa2738da8ca635253a5"
+    user: "kuklpsxjydnblu",
+    host: "ec2-99-80-170-190.eu-west-1.compute.amazonaws.com",
+    database: "d69013s2v5ealh", 
+    port: 5432,
+    password: "eef8b90546b3d90f332de10020bf44002ada6a142ee8eaa2738da8ca635253a5",
+    ssl:{
+        rejectUnauthorized : false,
+    }
+
 });
 
 module.exports = pool;
