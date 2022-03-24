4f7a86ee948c9ed419afb99d3ec5960e80f0bb0d
p0mmep0mme <93192139+p0mmep0mme@users.noreply.github.com>
2022-03-24 14:18:47 +0100

    postgress

--- database.js
+++ database.js
@@ -1,10 +1,11 @@
-const mysql = require("mysql");
+const Pool = require("pg").Pool;
 
-var connect = mysql.createConnection({
-    "host": "localhost",
-    "database": "eindwerkDB", 
-    "user": "root", 
-    "password": ""
+const pool = new Pool({
+    "host": "ec2-99-80-170-190.eu-west-1.compute.amazonaws.com",
+    "database": "d69013s2v5ealh", 
+    "user": "kuklpsxjydnblu",
+    "port":5432,
+    "password": "eef8b90546b3d90f332de10020bf44002ada6a142ee8eaa2738da8ca635253a5"
 });
 
-module.exports = connect;
+module.exports = pool;
