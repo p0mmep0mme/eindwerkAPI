089bbf527fc54af3b3c81d6e57d317d2d10a9fe3
p0mmep0mme <93192139+p0mmep0mme@users.noreply.github.com>
2022-03-25 14:05:34 +0100

    dranken update

--- routes/restricted-route.js
+++ routes/restricted-route.js
@@ -27,6 +27,7 @@ router.get("/dranken",basicAuth.authRouting.admin, mijndb.getAlldranken);
 router.get("/dranken/:id",basicAuth.authRouting.admin, mijndb.getDrankById);
 router.post("/dranken",basicAuth.authRouting.admin, mijndb.setDrank);
 router.put("/dranken/:id",basicAuth.authRouting.admin, mijndb.updateDrank);
+
 router.delete("/dranken/:id",basicAuth.authRouting.admin, mijndb.deleteDrank);
 
 
