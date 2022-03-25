f9447f6d0ca5c54c776782ef39ed5667b151cddb
p0mmep0mme <93192139+p0mmep0mme@users.noreply.github.com>
2022-03-25 12:27:11 +0100

    session.loggedin

--- basic-auth.js
+++ basic-auth.js
@@ -73,8 +73,8 @@ const authRouting = {
 function login(req, res){
     console.log(bestaatUser2)
     if(bestaatUser2 !== undefined){
-        // req.session.loggedin = true
-        // req.session.username = username
+        req.session.loggedin = true
+        req.session.username = username
         
         let payload = { "username" : req.body.username };
         let secret = "geheim";
